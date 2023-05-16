ef5253e2348b14175d364050ed8cd556c635620c
kalyanDurga <kalyandurga002@gmail.com>
2023-05-16 10:18:24 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,63 @@
+from typing import Any, Dict
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+from django.views.generic import View,TemplateView
+from app.forms import *
+
+def fbv_string(request):
+    return HttpResponse('this is function based view')
+
+class CBV_string(View):
+    def get(self,request):
+        return HttpResponse('This is class Based Viewww')
+    
+class fbv_html(View):
+    def get(self,request):
+        return render(request,'fbv_html.html')
+# this is function view for form
+
+def fbv_form(request):
+    TFO=Topicform()
+    WFO=Webpageform()
+    d={'TFO':TFO,'WFO':WFO}
+    if request.method=='POST':
+        TD=Topicform(request.POST)
+        WD=Webpageform(request.POST)
+        if TD.is_valid() and WD.is_valid():
+            TFD=TD.save(commit=False)
+            TFD.save()
+            WFD=WD.save(commit=False)
+            WFD.topic_name=TFD
+            WFD.save()
+            return HttpResponse('data submited')
+        return HttpResponse('data is not valid')
+
+    return render(request,'fbv_form.html',d)
+
+class CBV_form(View):
+    def get(self,request):
+        TFO=Topicform()
+        WFO=Webpageform()
+        d={'TFO':TFO,'WFO':WFO}
+        return render(request,'CBV_form.html',d)
+    def post(self,request):
+        TFD=Topicform(request.POST)
+        WFD=Webpageform(request.POST)
+        if TFD.is_valid() and WFD.is_valid():
+            TD=TFD.save(commit=False)
+            TD.save()
+
+            WD=WFD.save(commit=False)
+            WD.topic_name=TD
+            WD.save()
+            return HttpResponse('data inserteds')
+
+# this is template view
+
+            
+
+
+
+
